db231583618925b5cc1abbfa49967d8c0e47fbd5
TymoshokDmytro <tymoshok.dmytro@gmail.com>
2023-03-02 19:00:29 +0200

    Add async SSE events listener

--- app/alerts_api_service.py
+++ app/alerts_api_service.py
@@ -1,16 +1,23 @@
+import asyncio
 import os
 
+from aiohttp_sse_client import client as sse_client
+
 import requests
+from dotenv import load_dotenv
 from loguru import logger as log
 
 
-class AlertsAPIService:
-    """ API wrapper to receive one state information for https://alerts.com.ua/"""
+class BaseAlertsAPIService:
     __slots__ = ("base_url", "_api_key")
 
     def __init__(self, api_key: str):
         self.base_url = os.getenv("API_BASE_URL", "https://alerts.com.ua")
         self._api_key = api_key
+
+
+class AlertsAPIService(BaseAlertsAPIService):
+    """ API wrapper to receive one state information for https://alerts.com.ua/"""
 
     def check_state_request(self, state_number: int = 25):  # 25 = Kyiv
         if not isinstance(state_number, int):
@@ -30,3 +37,21 @@ class AlertsAPIService:
             log.error(f"State status request failed with error: {type(e)} {str(e)}")
 
         return
+
+
+class AlertsSSEService(BaseAlertsAPIService):
+
+    async def _async_sse_processing(self, state_number):
+        async with sse_client.EventSource(
+                url=f"{self.base_url}/api/states/live/{state_number}",
+                headers={"X-API-Key": self._api_key}
+        ) as event_source:
+            try:
+                async for event in event_source:
+                    print(event)
+            except ConnectionError:
+                pass
+
+    def process_sse_events(self, state_number: int = 25):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self._async_sse_processing(state_number))
